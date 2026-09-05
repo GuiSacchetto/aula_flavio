861b79551bc3fc0a6c94f54255d5b0d06f23c468
GuiSacchetto <guimota0305@gmail.com>
2026-09-05 00:17:00 +0000

    Salva projeto Java inicial

---
+++ aula_flavio/src/App.java
@@ -0,0 +1,73 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	   double altura,beeleza, carisma;
+	   Scanner ler = new Scanner(System.in);
+	
+	   System.out.println("Digite sua altura de 0 a 10");
+	   altura = ler.nextDouble();
+	   
+	   System.out.println("Digite sua beleza de 0 a 10");
+	   beleza = ler.nextDouble();
+	   
+	   System.out.println("Digite sua carisma de 0 a 10");
+	   carisma = ler.nextDouble();
+	   
+	   if (altura, beleza, carisma>7) {
+	       
+	   System.out.println("Boraaaaaa");    
+	   }
+	   
+	   if (altura>7 & beleza>7 & carisma<7) {
+	       
+	   System.out.println("Boraaaaaa");
+	   }
+	   
+	   if (altura<7 & beleza>7 & carisma>7) {
+	       
+	   System.out.println("Boraaaaaa");
+	   }
+	   
+	   if (altura>7 & beleza<7 & carisma>7) {
+	       
+	   System.out.println("Boraaaaaa");
+	   }
+
+       ---------------
+
+       import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    double nota, frequencia;
+	    Scanner ler = new Scanner(System.in);
+	
+	    System.out.println("Digite a sua nota");
+	    nota = ler.nextDouble();
+	    
+	    System.out.println("Digite a sua frequencia");
+	    frequencia = ler.nextDouble();
+	    
+	    if (nota>=7 & frequencia>0.75) {
+	        
+	    System.out.println("O aluno está aprovado");
+	    } else if (nota>8 || frequencia>=0.9) {
+	    
+	
+	    }else{
+	        
+	       System.out.println("O aluno está reprovado");
+	        
+	    }
+		
+	}
